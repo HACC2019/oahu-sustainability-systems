cdbf1e4affab2829212f2d0eb004644c2eda0fc9
willjsimmons <willjsimmons@gmail.com>
2019-10-30 12:26:14 -1000

    Added a template to components directory

--- app/imports/ui/components/Template.jsx
+++ app/imports/ui/components/Template.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class NotFound extends React.Component
+class ClassName extends React.Component
 {
     render(){
         const returnVal = <div></div>;
@@ -10,4 +10,4 @@ class NotFound extends React.Component
     }
 };
 
-export default NotFound;
+export default ClassName;
